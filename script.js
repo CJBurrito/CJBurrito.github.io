f5ccb9278878719b5d969db1e242199042262b22
Christian Balentine <christianbalentine47@gmail.com>
2020-03-22 01:55:44 -0400

    Leftover code for background image, unable to get working with Bulma. Come back later to fix with SASS

---
+++ script.js
@@ -0,0 +1,30 @@
+// function FindMyShowModal() {
+//     $ (".modal-card-title").text("Find My Show");
+
+// }
+
+// $ (".FindMyShow").on("click", modalOpen);
+
+// function modalOpen() {
+//     event.preventDefault();
+//     var modal = document.querySelector('.modal');  // assuming you have only 1
+//     var html = document.querySelector("html");
+//     modal.classList.add('is-active');
+//     html.classList.add('is-clipped');
+//     $('.modal-background').on('click', function (e) {
+//         e.preventDefault();
+//         $(".modal").removeClass('is-active');
+//         html.classList.remove('is-clipped');
+//     });
+//     $('.cancelBtn').on('click', function (e) {
+//         e.preventDefault();
+//         $(".modal").removeClass('is-active');
+//         html.classList.remove('is-clipped');
+//         $('.modal-card-body').empty();
+//         $(".modal-card-title").empty();
+//     });
+// }
+
+// // https://koolachoo.github.io/First-Project/
+// //https://cjburrito.github.io/Day-Planner/
+// //https://cjburrito.github.io/Password-Generator/
